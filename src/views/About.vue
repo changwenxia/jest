75d685519688010af24dd43d1ebd6a0fa40402af
changwenxia <13522695700@163.com>
2020-03-10 15:30:29 +0800

    feat: 测试用例

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,12 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    {{ msg }}
   </div>
 </template>
+<script>
+export default {
+  props: ["msg"],
+
+}
+</script>
--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,12 +1,56 @@
-import { shallowMount } from '@vue/test-utils'
-import HelloWorld from '@/components/HelloWorld.vue'
+import {mount} from '@vue/test-utils'
+import HelloWorld from '@/components/HelloWorld'
+import MyButton from '@/components/button'
 
-describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'new message'
-    const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg }
-    })
-    expect(wrapper.text()).toMatch(msg)
+describe('测试Vue组件实例',()=>{
+  const wrapper = mount(HelloWorld);
+  it("test props", ()=>{
+    wrapper.setProps( {msg: "hello"})
+    expect(wrapper.vm.msg).toBe("hello")
   })
+  it("test data", ()=>{
+    const title = "i am title in test"
+    wrapper.setData( {title: title})
+    expect(wrapper.vm.title).toBe(title)
+  })
+  it("test dom", ()=>{
+    // expect(wrapper.find('.hello h1').text()).toBe('hello')
+    console.log(wrapper.find(".hello h1").name());
+    expect(wrapper.find(".hello h1").is('div'))
+  })
+
+})
+
+describe('测试Vue组件实例',()=>{
+  const wrapper = mount(MyButton);
+  it("test events", ()=>{
+    const mockFn = jest.fn();
+    wrapper.setMethods({
+      increment: mockFn
+    })
+    wrapper.find("button").trigger("click")
+    expect(mockFn).toBeCalled();
+    // 查看是否有回调和回调次数
+    expect(mockFn).toHaveBeenCalledTimes(1)
+  })
+})
+
+
+test('测试jest.fn()调用', () => {
+  let mockFn = jest.fn();
+  let result = mockFn(1, 2, 3);
+
+  expect(mockFn).toBeCalled();
+  expect(mockFn).toHaveBeenCalledWith(1, 2, 3);
+})
+
+test('测试jest.fn()返回固定值', () => {
+  let mockFn = jest.fn().mockReturnValue('default');
+  expect(mockFn()).toBe('default');
+
+  let mockFn1 = jest.fn((num1, num2) => {
+    return num1 * num2;
+  })
+  expect(mockFn1(10, 10)).toBe(100);
+  
 })
